2f47f6ff0bec3af6d593078f22dba131594f3ec0
akashlearns <Akash.Yadav.software.engineer@gmail.com>
2025-02-25 16:11:20 +0530

    ArrayLargetno.Brute.R

---
+++ Revision/p25feb.java
@@ -0,0 +1,11 @@
+package Revision;
+import java.util.Arrays;
+//Largest no. of array
+public class p25feb {
+    public static void main(String[] args) {
+        int[] arr= {1,3,5,7,9};
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length-1]);
+
+    }
+}
