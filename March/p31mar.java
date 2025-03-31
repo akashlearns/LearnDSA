657f9df986fb53b783091411ccb1e3a2312d0623
akashlearns <Akash.Yadav.software.engineer@gmail.com>
2025-03-31 15:08:33 +0530

    linked List Practice ffor the first time.

---
+++ March/p31mar.java
@@ -0,0 +1,12 @@
+package March;
+//linked List Practice ffor the first time.
+public class p31mar {
+    public static class Node{
+        int data;
+        Node next;
+    }
+    public static void main(String[] args) {
+        Node x = new Node();
+        System.out.println(x.next);
+    }
+}
